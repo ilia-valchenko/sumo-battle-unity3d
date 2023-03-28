b896860e7a792078bae07f26922dbc64f69ad35b
ivalchanka <i.valchenk@hotmail.com>
2023-03-28 22:13:00 +0400

    Made some changes.

---
+++ sumo-battle/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotationSpeed = 120f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //// Alternative
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    this.transform.Rotate(Vector3.up, 1);
+        //    //this.transform.RotateAround()
+        //}
+        //else if (Input.GetKey(KeyCode.A))
+        //{
+        //    this.transform.Rotate(Vector3.up, -1);
+        //}
+
+
+
+        // Requires to use arrows on your keyboard
+        // "Horizontal" is case-sensitive
+        var horizantalInput = Input.GetAxis("Horizontal");
+        this.transform.Rotate(Vector3.up, horizantalInput * this.rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ sumo-battle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidBody;
+    private GameObject focalPoint;
+
+    public float playerSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.playerRigidBody = GetComponent<Rigidbody>();
+        this.focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var verticalInput = Input.GetAxis("Vertical");
+        // What about Time.deltaTime???
+        //this.playerRigidBody.AddForce(Vector3.forward * this.playerSpeed * verticalInput);
+        this.playerRigidBody.AddForce(this.focalPoint.transform.forward * this.playerSpeed * verticalInput);
+    }
+}
