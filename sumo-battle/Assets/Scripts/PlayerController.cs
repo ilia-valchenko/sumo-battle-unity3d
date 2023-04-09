b3ca6c2b7eba56703173bf86cc8be61d64d736cc
ivalchanka <i.valchenk@hotmail.com>
2023-04-09 17:53:15 +0400

    Finished implementing powerup indicator.

--- sumo-battle/Assets/Scripts/PlayerController.cs
+++ sumo-battle/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private const float PlayerSpeed = 3.5f;
     private const float PowerUpForce = 15.0f;
+    private const float CountdownLimitInSeconds = 7.0f;
 
     private Rigidbody playerRigidBody;
     private GameObject focalPoint;
 
     public bool hasPowerup = false;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +27,7 @@ public class PlayerController : MonoBehaviour
         // What about Time.deltaTime???
         //this.playerRigidBody.AddForce(Vector3.forward * this.playerSpeed * verticalInput);
         this.playerRigidBody.AddForce(this.focalPoint.transform.forward * PlayerSpeed * verticalInput);
+        this.powerupIndicator.gameObject.transform.position = this.transform.position + new Vector3(0, -0.5f, 0);
     }
 
     public void OnTriggerEnter(Collider other)
@@ -32,6 +36,8 @@ public class PlayerController : MonoBehaviour
         {
             this.hasPowerup = true;
             Destroy(other.gameObject);
+            this.powerupIndicator.gameObject.SetActive(true);
+            StartCoroutine(PowerupCountdownRoutine());
         }
     }
 
@@ -43,5 +49,12 @@ public class PlayerController : MonoBehaviour
             var awayFromPlayer = enemyRigidBody.position - this.playerRigidBody.position;
             enemyRigidBody.AddForce(awayFromPlayer * PowerUpForce, ForceMode.Impulse);
         }
+    }
+
+    private IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(CountdownLimitInSeconds);
+        this.hasPowerup = false;
+        this.powerupIndicator.gameObject.SetActive(false);
     }
 }
